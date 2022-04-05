bde606362c80e70239717ab72aeaae3b8e2563d4
Flávio Mira <Flaviomira13@gmail.com>
2022-04-05 13:46:04 -0300

    Adjustiments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 function getCupon(nome) {
-    var mensage = "<div id='cupon-text'><h1>Uhuul!!! Ótima escolha. O " + nome + " é um dos mais pedidos! <br> Pegue agora mesmo o seu código promocional!!</h1><div>";
+    var mensage = "<div id='cupon-text'><h1>Uhuul!!! Ótima escolha. O <span class='destaque'>" + nome + "</span> é um dos mais pedidos! <br> Pegue agora mesmo o seu código promocional!!</h1><div>";
     var cuponCode = "<div id='cupon-code'></div>";
+    var buttonExit = "<button type='button' onclick='fechar()'><img src='img/closeIcon.png'></button>";
     var elemento = document.getElementById("cupons-wrapper");
     elemento.style.height = 250 + 'px';
-    elemento.innerHTML = mensage + cuponCode;
+    elemento.innerHTML = mensage + cuponCode + buttonExit;
 }
 
 function makeActive(tag){
     lista = document.getElementsByTagName("li");
     for(i=0;i<lista.length;i++){      
-        lista[i].style.background = "#fed8b5";
+        lista[i].classList.remove("ativo");      
+        lista[i].classList.add("inativo");
     }
-    tag.style.background = "#FE9738";
+    tag.classList.remove("inativo");
+    tag.classList.add("ativo");
+}
+
+function fechar(){
+    var elementoFechar = document.getElementById("cupons-wrapper");
+    elementoFechar.style.height = 0 + 'px';
+    elementoFechar.style.transition = "height 1s";
+    
+    lista = document.getElementsByTagName("li");
+    for(i=0;i<lista.length;i++){      
+        lista[i].classList.remove("ativo");      
+        lista[i].classList.add("inativo");
+    }
 }
