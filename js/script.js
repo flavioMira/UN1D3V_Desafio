49bff3a3b7e84019a6890cad6a9681e7969ee29d
Flávio Mira <Flaviomira13@gmail.com>
2022-04-05 16:34:08 -0300

    Code and final adjustments

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
 function getCupon(nome) {
-    var mensage = "<div id='cupon-text'><h1>Uhuul!!! Ótima escolha. O <span class='destaque'>" + nome + "</span> é um dos mais pedidos! <br> Pegue agora mesmo o seu código promocional!!</h1><div>";
-    var cuponCode = "<div id='cupon-code'></div>";
+    var mensage = "<div id='cupon-text'><h1>Uhuul!!! Ótima escolha. O <span class='destaque'>" + nome + "</span> é um dos mais pedidos! <br> Pegue agora mesmo o seu código promocional!!</h1></div>";
+    var cuponCode = "<div id='cupon-code'><div id='cupon-id'></div></div>";
     var buttonExit = "<button type='button' onclick='fechar()'><img src='img/closeIcon.png'></button>";
     var elemento = document.getElementById("cupons-wrapper");
     elemento.style.height = 250 + 'px';
     elemento.innerHTML = mensage + cuponCode + buttonExit;
+    codigo(nome);
+}
+
+function codigo(nome) {
+    var cuponID = document.getElementById("cupon-id");
+    var codigo;
+        switch (nome) {
+            case "BigDev":
+                codigo = "#199BD";
+                break;
+            case "DevChiken":
+                codigo = "#990DC";
+                break;
+            case "DevCheddar":
+                codigo = "#199DC";
+                break;
+            case "DevTasty":
+                codigo = "#249DT";
+                break;
+            case "DevNífico":
+                codigo = "#199DN";
+                break;
+            case "DevSquad":
+                codigo = "#199DS";
+        
+            default:
+                codigo: "#ERROR";
+                break;
+        }
+        
+    cuponID.innerHTML =  codigo;
 }
 
 function makeActive(tag){
@@ -21,6 +52,7 @@ function fechar(){
     var elementoFechar = document.getElementById("cupons-wrapper");
     elementoFechar.style.height = 0 + 'px';
     elementoFechar.style.transition = "height 1s";
+    elementoFechar.innerHTML = "";
     
     lista = document.getElementsByTagName("li");
     for(i=0;i<lista.length;i++){      
